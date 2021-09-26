178fb541e17f3492c4b73a0e455cec3f08b03167
Asbjørn Enge <asbjorn@hanafjedle.net>
2021-09-26 10:22:59 +0200

    Doing batches of 300 transfers

--- send.js
+++ send.js
@@ -10,18 +10,15 @@ import {
 const FAUCET_KEY = JSON.parse(fs.readFileSync(`../tezid-secrets/${AIRDROP_ADMIN}.json`).toString())
 const tezos = new TezosToolkit(TEZOS_RPC)
 importKey(tezos,
-  FAUCET_KEY.email,
-  FAUCET_KEY.password,
-  FAUCET_KEY.mnemonic.join(' '),
-  FAUCET_KEY.secret
-).catch((e) => console.error(e))
+  FAUCET_KEY.privkey
+).catch((e) => console.error(e));
 
 const weighted = JSON.parse(fs.readFileSync('weighted.json'))
-const batchSize = 500
+const batchSize = 300
 
 function storeWeighted() {
   fs.writeFileSync('weighted.json', JSON.stringify(weighted))
-  fs.writeFileSync('weighted-backup.json', JSON.stringify(weighted))
+//  fs.writeFileSync('backup.json', JSON.stringify(weighted))
 }
 function getWeightedList() {
   return Object.keys(weighted).map(address => {
@@ -41,16 +38,42 @@ async function send() {
   const batch = notSendt.splice(0,batchSize)
   try {
     await sleep(3)
+    let transferList = batch.map(receiver => {
+      return {
+        from_: AIRDROP_ADMIN, 
+        txs: [
+          {
+            to_: receiver.address, 
+            token_id: 0,
+            amount: Math.trunc(receiver.tokens * 10**8) 
+          }
+        ]
+      }
+    })
+    let contract = await tezos.contract.at(IDZ_CONTRACT)
 
+    let estimateOp = await contract.methods.transfer(transferList).toTransferParams({})
+    let { gasLimit, storageLimit, suggestedFeeMutez } = await tezos.estimate.transfer(estimateOp)
+    let op = await contract.methods.transfer(transferList).send({
+      fee: suggestedFeeMutez,
+      gasLimit: gasLimit,
+      storageLimit: storageLimit
+    })
+
+    //let op = await contract.methods.transfer(transferList).send()
+
+    await op.confirmation(1)
+    console.log(op.hash)
     batch.forEach(b => {
       weighted[b.address].transferred = true
+      weighted[b.address].transaction = op.hash 
     })
     storeWeighted()
     const stillNotSendt = getWeightedList().filter(w => !w.transferred)
-    console.log('Still not sendt', stillNotSendt.length)
+    await sleep(30)
     if (stillNotSendt.length > 0) await send()
   } catch(err) {
-    throw err
+    console.error(err)
   }
 }
 
