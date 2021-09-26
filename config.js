cd37528745d1b52e70597f630f60a355f83faed2
Asbjørn Enge <asbjorn@hanafjedle.net>
2021-09-26 09:42:20 +0200

    ENV VAR fix

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-export const AIRDROP_ADMIN    = process.env['TEZID_ADMIN']  || ''
-export const IDZ_CONTRACT     = process.env['IDZ_CONTRACT'] || ''
-export const TEZOS_RPC        = process.env['TEZOS_RPC']    || ''
+export const AIRDROP_ADMIN    = process.env['AIRDROP_ADMIN']  || ''
+export const IDZ_CONTRACT     = process.env['IDZ_CONTRACT']   || ''
+export const TEZOS_RPC        = process.env['TEZOS_RPC']      || ''
